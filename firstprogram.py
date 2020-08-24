35b13fd4a4085ef2ad95a1a0b7089fca4c5b7f01
yashthetiger <64192060+yashthetiger@users.noreply.github.com>
2020-08-24 21:09:48 +0530

    create first.py

---
+++ firstprogram.py
@@ -0,0 +1,9 @@
+//Area of circle
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+//Extension of the file
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print("The extension of the file is : 'Python'")
